13152e36556d9d5140f822fb9fff5e499f939106
becausemirbek <becausemirbek@gmail.com>
2022-01-15 15:02:33 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TasksPage from "./views/TasksPage/TasksPage";
+import "./App.scss";
+function App() {
+  return (
+    <div className="App">
+      <TasksPage />
+    </div>
+  );
+}
+
+export default App;
